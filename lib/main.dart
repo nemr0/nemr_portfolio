520a81cf0d571a4249ceeec9fc017779727c6be6
Omar El Nemr <60761933+nemr0@users.noreply.github.com>
2023-04-09 03:40:18 +0200

    added thumbnail, meta_seo package

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:meta_seo/meta_seo.dart';
 import 'package:nemr_portfolio/UI/main_screen.dart';
 import 'package:seo/seo.dart';
 import 'UI/style/theme.dart';
 
 Future<void> main() async {
   await GetStorage.init();
+  // Define MetaSEO object
+  MetaSEO meta = MetaSEO();
+  // add meta seo data for web app as you want
+  meta.author(author: 'Omar Elnemr');
+  meta.description(description: 'Omar Elnemr\'s Portfolio.');
+  meta.keywords(
+      keywords:
+          'Flutter, Developer, Mobile Apps Developer, Flutter Developer, Flutter Mobile, Omar Elnemr');
+  meta.nameContent(name: 'Omar Elnemr', content: 'Flutter Developer Portfolio');
+  // meta.ogImage(ogImage: '')
   runApp(const ProviderScope(
     child: MyApp(),
   ));
