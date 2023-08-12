4f23e82e13d2d78cbe26b9079e5730c42281cc54
Omar El Nemr <60761933+nemr0@users.noreply.github.com>
2023-08-12 21:54:02 +0300

    removed page view physics and magnified icon button tap size using inkwell instead.

--- lib/UI/widgets/window_widgets/projects/project_list.dart
+++ lib/UI/widgets/window_widgets/projects/project_list.dart
@@ -28,54 +28,83 @@ class ProjectList extends HookWidget {
         children: [
           Positioned.fill(
             child: PageView.builder(
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: configs.length,
               controller: projectCTR,
               onPageChanged: onPageChanged,
               itemBuilder: (_, i) {
-                return Transform.scale(
-                  scale: i == currentIndex ? 1 : 0.9,
-                  child: ProjectWidget(
-                    config: configs[i],
-                    last: i == configs.length - 1,
-                  ),
+                return ProjectWidget(
+                  config: configs[i],
+                  last: i == configs.length - 1,
+                  clickable: i == currentIndex,
                 );
               },
             ),
           ),
-          if (currentIndex != configs.length - 1)
-            Align(
-              alignment: Alignment.centerLeft,
-              child: IconButton(
-                onPressed: () {
-                  projectCTR.nextPage(
-                    duration: const Duration(milliseconds: 100),
-                    curve: Curves.bounceIn,
-                  );
-                },
-                icon: const FaIcon(
-                  FontAwesomeIcons.solidCircleLeft,
-                  size: 20,
-                  color: CupertinoColors.white,
+          Positioned.fill(
+              child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (currentIndex != configs.length - 1)
+                Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () => projectCTR.nextPage(
+                      duration: const Duration(milliseconds: 100),
+                      curve: Curves.bounceIn,
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: const BoxDecoration(
+                            color: CupertinoColors.black,
+                            shape: BoxShape.circle),
+                        child: const FaIcon(
+                          FontAwesomeIcons.solidCircleLeft,
+                          size: 30,
+                          color: CupertinoColors.white,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
+              if (currentIndex == configs.length - 1)
+                const Expanded(flex: 2, child: SizedBox()),
+              const Spacer(
+                flex: 3,
               ),
-            ),
-          if (currentIndex != 0)
-            Align(
-              alignment: Alignment.centerRight,
-              child: IconButton(
-                onPressed: () {
-                  projectCTR.previousPage(
-                    duration: const Duration(milliseconds: 100),
-                    curve: Curves.bounceIn,
-                  );
-                },
-                icon: const FaIcon(
-                  FontAwesomeIcons.solidCircleRight,
-                  size: 20,
-                  color: CupertinoColors.white,
+              if (currentIndex == 0) const Expanded(flex: 2, child: SizedBox()),
+              if (currentIndex != 0)
+                Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () => projectCTR.previousPage(
+                      duration: const Duration(milliseconds: 100),
+                      curve: Curves.bounceIn,
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: const BoxDecoration(
+                            color: CupertinoColors.black,
+                            shape: BoxShape.circle),
+                        child: const FaIcon(
+                          FontAwesomeIcons.solidCircleRight,
+                          size: 30,
+                          color: CupertinoColors.white,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
+            ],
+          )),
         ],
       ),
     );
--- lib/UI/widgets/window_widgets/projects/project_widget.dart
+++ lib/UI/widgets/window_widgets/projects/project_widget.dart
@@ -16,92 +16,114 @@ import '../../buttons/link_button.dart';
 import '../../glass_morphism.dart';
 
 class ProjectWidget extends HookWidget {
-  const ProjectWidget({Key? key, required this.config, this.last = false})
-      : super(key: key);
+  const ProjectWidget({
+    Key? key,
+    this.clickable = false,
+    required this.config,
+    this.last = false,
+  }) : super(key: key);
+  final bool clickable;
   final ProjectConfig config;
   final bool last;
   @override
   Widget build(BuildContext context) {
-    return last
-        ? InkWell(
-            onTap: () => onLinkLaunch(kGithubLinkButtonConfig.link!),
-            child: GradientBorderGlassBox(
-              radius: 10,
-              height: 300,
-              width: context.width,
-              inColor: kAltContainerColor,
-              onlyTopRadius: false,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'More?',
-                    style: kTSTitle,
-                  ),
-                  LinkButton(config: kGithubLinkButtonConfig),
-                ],
-              ),
-            ),
-          )
-        : InkWell(
-            mouseCursor: SystemMouseCursors.click,
-            onTap: () {
-              context.goNamed(
-                config.id,
-                // queryParameters: {}
-              );
-            },
-            child: Stack(
-              children: [
-                GradientBorderGlassBox(
-                    radius: 10,
-                    height: 300,
-                    width: context.width,
-                    inColor: kAltContainerColor,
-                    onlyTopRadius: false,
-                    child: usePrecacheFadeInImage(FadeInImage.memoryNetwork(
-                        placeholder: kTransparentImage,
-                        image: config.url,
-                        fit: BoxFit.cover,
-                        imageErrorBuilder: (ctx, _, __) => Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                FaIcon(
-                                  FontAwesomeIcons.triangleExclamation,
-                                  size: 30,
-                                  color:
-                                      Theme.of(ctx).colorScheme.errorContainer,
-                                ),
-                                Text(
-                                  'Could Not Load Image',
-                                  style: CupertinoTheme.of(context)
-                                      .textTheme
-                                      .textStyle
-                                      .copyWith(
-                                        color: Theme.of(ctx).colorScheme.error,
-                                      ),
-                                ),
-                              ],
-                            )))),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: GlassMorphism(
-                    width: context.width,
-                    borderRadius: const BorderRadius.vertical(
-                      bottom: Radius.circular(10),
-                    ),
-                    color: CupertinoColors.black.withOpacity(.7),
-                    child: Text(
-                      config.name,
-                      style: kTSBody,
-                    ),
+    return Opacity(
+      opacity: clickable ? 1 : .5,
+      child: Transform.scale(
+        scale: clickable ? 1 : 0.9,
+        child: last
+            ? InkWell(
+                mouseCursor: !clickable
+                    ? SystemMouseCursors.basic
+                    : SystemMouseCursors.click,
+                onTap: clickable
+                    ? () => onLinkLaunch(kGithubLinkButtonConfig.link!)
+                    : null,
+                child: GradientBorderGlassBox(
+                  radius: 10,
+                  height: 300,
+                  width: context.width,
+                  inColor: kAltContainerColor,
+                  onlyTopRadius: false,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'More?',
+                        style: kTSTitle,
+                      ),
+                      LinkButton(config: kGithubLinkButtonConfig),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          );
+              )
+            : InkWell(
+                mouseCursor: !clickable
+                    ? SystemMouseCursors.basic
+                    : SystemMouseCursors.click,
+                onTap: clickable
+                    ? () {
+                        context.goNamed(
+                          config.id,
+                          // queryParameters: {}
+                        );
+                      }
+                    : null,
+                child: Stack(
+                  children: [
+                    GradientBorderGlassBox(
+                        radius: 10,
+                        height: 300,
+                        width: context.width,
+                        inColor: kAltContainerColor,
+                        onlyTopRadius: false,
+                        child: usePrecacheFadeInImage(FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: config.url,
+                            fit: BoxFit.cover,
+                            imageErrorBuilder: (ctx, _, __) => Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.triangleExclamation,
+                                      size: 30,
+                                      color: Theme.of(ctx)
+                                          .colorScheme
+                                          .errorContainer,
+                                    ),
+                                    Text(
+                                      'Could Not Load Image',
+                                      style: CupertinoTheme.of(context)
+                                          .textTheme
+                                          .textStyle
+                                          .copyWith(
+                                            color:
+                                                Theme.of(ctx).colorScheme.error,
+                                          ),
+                                    ),
+                                  ],
+                                )))),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: GlassMorphism(
+                        width: context.width,
+                        borderRadius: const BorderRadius.vertical(
+                          bottom: Radius.circular(10),
+                        ),
+                        color: CupertinoColors.black.withOpacity(.7),
+                        child: Text(
+                          config.name,
+                          style: kTSBody,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
   }
 }
